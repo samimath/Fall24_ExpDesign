15a877d6366a808f70c2a0ad54595a757def1aa2
Sami Cheong <samicheong@gmail.com>
2024-10-24 17:56:23 -0500

    foldover design comparison

--- lecture15/compre_foldover.R
+++ lecture15/compre_foldover.R
@@ -1,20 +1,23 @@
 ### script to compare foldover designs:
 
-## original design:
-des_test <- FrF2( nruns = 8,  
+## original design 2^{7-4}:
+des_test1 <- FrF2( nruns = 8,  
                   nfactors = 7, 
                   generator = c('AB','AC','BC','ABC'))
-## 
-des_test2<- fold.design(des_test, columns = c(3))
+## design 2: fold over around column C 
+des_test2<- fold.design(des_test1, columns = c(3))
 
-## 
-des_test3<- fold.design(des_test, columns = c(3))
-aliases(lm(y~(.)^2, data = des_test2))
+## design 3: fold over around columns E = AC,F=BC,G=ABC
+des_test3<- fold.design(des_test, columns = c(5,6,7))
 
-des_test4<- fold.design(des_test, columns = 'full')
+## design 4: fold over around columns C, E = AC,F=BC,G=ABC
+des_test4<- fold.design(des_test, columns = c(3,5,6,7))
+
+## design 5: fold over around all the columns
+des_test6<- fold.design(des_test, columns = 'full')
 
 
-## write a function to compare the design info from each design object
+## write a function to compare the main and two-way aliases from each design object
 
 compare_designs<-function(design){
   
@@ -24,7 +27,15 @@ compare_designs<-function(design){
 }
 
 
-compare_designs(design = des_test )
-compare_designs(design = des_test2 )
-compare_designs(design = des_test3 )
-compare_designs(design = des_test4 )
+
+
+k=0
+for(i in list(des_test1,des_test2,des_test3,
+              des_test4,des_test5,des_test6)){
+  k <- k+1
+  print(paste('design',k))
+  output <- compare_designs(design = i )
+  
+  print(output)
+}
+
